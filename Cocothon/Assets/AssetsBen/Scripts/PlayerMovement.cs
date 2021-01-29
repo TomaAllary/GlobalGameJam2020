47a2e2201b1f7a8a0cfa90dcb14e80856aac058d
allaryt <toma.allary@uqtr.ca>
2021-01-29 10:19:11 -0500

    Made karen slower when following timmy and colliding with others

--- Cocothon/Assets/AssetsBen/Scripts/PlayerMovement.cs
+++ Cocothon/Assets/AssetsBen/Scripts/PlayerMovement.cs
@@ -40,24 +40,11 @@ public class PlayerMovement : MonoBehaviour
                 direction = (horizontalInput * Vector3.right + verticalInput * Vector3.forward).normalized;
                 transform.LookAt(transform.position + direction);
 
-
-                //var oldPosition = transform.position;
-
                 rb.MovePosition(transform.position + direction * Time.deltaTime * speed);
             }
-      
-            
-             //This is for the active character rotation.. It kinda works, the character turns when you press left or right instead of straffing, but it's WAYYYY too sensitive..
-            /*
-            var newPosition = transform.position;
-            var headingDirection = newPosition - oldPosition;
-            Quaternion LookAtRotation = Quaternion.LookRotation(headingDirection);
-            Quaternion LookAtRotationOnly_Y = Quaternion.Euler(transform.rotation.eulerAngles.x, LookAtRotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-            transform.rotation = LookAtRotationOnly_Y;
-            */
 
 
-
+            //limit of map -> should be walls
             if (transform.position.x < -xRange)
             {
                 transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -81,15 +68,25 @@ public class PlayerMovement : MonoBehaviour
 
         //Make Karen follow Timmy
         else if(gameObject.name == "Karen" &&  ((timmy.transform.position - transform.position).magnitude > 10))
-        {           
-            transform.position = Vector3.Lerp(transform.position, timmy.transform.position - new Vector3(10*(Math.Sign(transform.position.x - timmy.transform.position.x)),0,10 * (Math.Sign(transform.position.z - timmy.transform.position.z))), Time.deltaTime );
-            transform.rotation = Quaternion.Lerp(transform.rotation, timmy.transform.rotation, Time.deltaTime*100);
+        {
+            Vector3 diff = timmy.transform.position - transform.position;
+            //ignore y diff
+            diff.y = 0;
+            diff.Normalize();
+
+            transform.LookAt(transform.position + diff);
+            rb.MovePosition(transform.position + diff * Time.deltaTime * speed);
         }
 
         else if (gameObject.name == "Timmy" && ((karen.transform.position - transform.position).magnitude > 10))
         {
-            transform.position = Vector3.Lerp(transform.position, karen.transform.position - new Vector3(10 * (Math.Sign(transform.position.x - karen.transform.position.x)), 0, 10 * (Math.Sign(transform.position.z - karen.transform.position.z))), Time.deltaTime);
-            transform.rotation = Quaternion.Lerp(transform.rotation, karen.transform.rotation, Time.deltaTime*100);
+            Vector3 diff = karen.transform.position - transform.position;
+            //ignore y diff
+            diff.y = 0;
+            diff.Normalize();
+
+            transform.LookAt(transform.position + diff);
+            rb.MovePosition(transform.position + diff * Time.deltaTime * speed);
         }
 
     }
