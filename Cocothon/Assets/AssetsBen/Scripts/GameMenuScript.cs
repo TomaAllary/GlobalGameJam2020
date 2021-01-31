c7175c5d9d73df7ea3574ef3d9cd63494bbda014
matteabe <benoit.matteau@uqtr.ca>
2021-01-31 12:44:40 -0500

    Finished Game menu script
    Still must link button when we can work on main scene

--- Cocothon/Assets/AssetsBen/Scripts/GameMenuScript.cs
+++ Cocothon/Assets/AssetsBen/Scripts/GameMenuScript.cs
@@ -2,27 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameMenuScript : MonoBehaviour
 {
-    Button startButton;
-    Button restartButton;
-    Button quitButton;
+    public Button startButton;
+    public Button restartButton;
+    public Button quitButton;
     public GameObject menu;
 
     // Start is called before the first frame update
     void Start()
     {
         menu.SetActive(false);
-        if(Langue.LangueSelectionnee == "Francais")
+        if (Langue.LangueSelectionnee == "Francais")
         {
+            startButton.GetComponentInChildren<Text>().text = "Revenir";
+            restartButton.GetComponentInChildren<Text>().text = "Recommencer";
+            quitButton.GetComponentInChildren<Text>().text = "Quitter";
 
+        }
+        else
+        {
+            startButton.GetComponentInChildren<Text>().text = "Resume";
+            restartButton.GetComponentInChildren<Text>().text = "Restart";
+            quitButton.GetComponentInChildren<Text>().text = "Quit";
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void resumeGame()
+    {
+        menu.SetActive(false);
+    }
+
+    public void quitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
