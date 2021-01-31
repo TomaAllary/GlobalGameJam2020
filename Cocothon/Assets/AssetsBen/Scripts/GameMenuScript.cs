2ceb3212c4e2aef678ea6c95a63682845ab4dd66
matteabe <benoit.matteau@uqtr.ca>
2021-01-31 12:13:15 -0500

    Added a game menu Not finished yet

---
+++ Cocothon/Assets/AssetsBen/Scripts/GameMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenuScript : MonoBehaviour
+{
+    Button startButton;
+    Button restartButton;
+    Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Langue.LangueSelectionnee == "Francais")
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
